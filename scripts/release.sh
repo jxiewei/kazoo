345e312bf2241f80596d95c60e65f1225fcd4a82
jxiewei <jxiewei@gmail.com>
2014-08-24 11:26:45 +0800

    Added version file to release tar.

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,6 +3,8 @@
 
 VER=`git rev-parse --short HEAD`
 RELFILE="release-${VER}.tar"
+echo $VER > version
+tar -rf ${RELFILE} version
 find ./ -name ebin -type d -exec tar -rf ${RELFILE} {} ';'
 find ./ -name priv -type d -exec tar -rf ${RELFILE} {} ';'
 tar -rf ${RELFILE} scripts utils
